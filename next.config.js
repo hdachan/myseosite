e11db22459022dd3c8b7357dc4013e46ae2b5afe
황대찬 <ghkdeocks123@naver.com>
2025-12-27 16:22:06 +0900

    20251227 기존세팅 파일 수정

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "dynamic-media-cdn.tripadvisor.com",
+      },
+      // 나중에 직접 촬영한 사진을 AWS S3나 Cloudinary 쓰면 여기에 추가하면 끝!
     ],
   },
   experimental: {
